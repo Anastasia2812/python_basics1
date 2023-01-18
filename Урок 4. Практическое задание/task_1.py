412e7109254ab9bc4949438bbc1c5a19648229a7
Anastasia2812 <122151115+Anastasia2812@users.noreply.github.com>
2023-01-18 22:54:37 +0300

    task_4_1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,14 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+my_name, my_time, my_salary, my_bonus = argv
+try:
+    my_time = int(my_time)
+    my_salary = int(my_salary)
+    my_bonus = int(my_bonus)
+    my_res = my_time * my_salary + my_bonus
+    print(f'заработная плата сотрудника  {res}')
+except ValueError:
+    print('Not a number')
